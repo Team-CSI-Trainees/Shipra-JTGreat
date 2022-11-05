fbd7dc874bea42a427672f7e7b63be51b71328d2
Shipra <shipratripathi.500@gmail.com>
2022-11-06 02:12:33 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/hero/Hero'
+import Kitchen from './components/kitchen/Kitchen'
+import Items from './components/items/Items'
+import Video from './components/video/Video'
+import Connect from './components/connect/Connect'
+import Footer from './components/footer/Footer'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+   <Navbar />
+   <Hero />
+   <Kitchen />
+   <Items/>
+   <Video />
+   <Connect />
+   <Footer />
+   
+   </div>
+   )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -14,4 +14,13 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();                         
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(
+//   <h1>hello</h1>,
+//   document.getElementById('root')
+
+// );
